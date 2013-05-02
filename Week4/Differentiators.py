ae67e409dae4785e2b46ea384177c9471369b3d3
Chad Rexrode <crexrode@calpoly.edu>
2013-05-01 19:40:50 -0700

    Differentiation Exercises

--- Week4/Differentiators.py
+++ Week4/Differentiators.py
@@ -12,10 +12,8 @@ def fourPtFiniteDiff(x,y):
     """Returns the four point finite difference derivative of y with respect to x"""
     dydx=np.zeros(y.shape,float)
     dydx[0]=(y[1]-y[0])/(x[1]-x[0])
-    dydx[1]=(y[2]-y[1])/((x[2] - x[1]))
+    dydx[1]=(y[2]-y[1])/(x[2]-x[1])
     dydx[-1]=(y[-1]-y[-2])/(x[-1]-x[-2])
-    dydx[-2]=(y[-2]-y[-3])/((x[-2] - x[-3]))
-    for i, l in enumerate(y[2:-2]):
-        i+=2
-        dydx[i] = (y[i-2]-8*y[i-1]+8*y[i+1]-y[i+2])/(12*(x[i+1]-x[i]))
+    dydx[-2]=(y[-2]-y[-3])/(x[-2]-x[-3])
+    dydx[2:-3]=(y[0:-5]-(8.*y[1:-4])+(8.*y[3:-2])-y[4:-1])/(12*(x[2:-3]-x[1:-4]))
     return dydx
